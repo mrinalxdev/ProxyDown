c408222a5bcfd96d4034be68c565dff1a3ae74a7
Hi_Mrinal <mrinalbitsat@gmail.com>
2022-11-16 21:02:52 +0530

    Level 1 project

---
+++ JsPlayground/PassengerCounter/index.js
@@ -0,0 +1,25 @@
+let count = 0
+let saveWord = document.getElementById("saveEl")
+
+function plus () {
+    count = count + 1
+    document.getElementById("main-count").innerText = count
+}
+
+function minus() {
+    count -= 1 
+
+    if (count < 0) {
+        alert("There no passengers in here !")
+    }
+    else {
+    document.getElementById("main-count").innerText = count  
+    }
+}
+
+function save() {
+    let saveFunc = count + " - "
+    saveWord.textContent += saveFunc
+    count.textContent = 0
+    count = 0
+}
