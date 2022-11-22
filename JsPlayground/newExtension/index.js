4e5317145cf699b69a149ede8fa590b505bf4e17
Hi_Mrinal <mrinalbitsat@gmail.com>
2022-11-22 12:06:39 +0530

    Added some Js to the Project

--- JsPlayground/newExtension/index.js
+++ JsPlayground/newExtension/index.js
@@ -1,5 +1,20 @@
-let btnsave = document.getElementById("input-btn")
+myLeads = [] 
+const inputEl = document.getElementById("input-el")
+const btnsave = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-btnsave.addEventListener( "click", function (){
-    console.log("happy to be Developer")
+btnsave.addEventListener("click", function (){
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 })
+
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0 ; i < myLeads.length ; i ++){
+        listItmes = "<li><a href='"+ myLeads[i] +"' target = '_blank'>"+ myLeads[i] + "</a></li> "
+    }
+    ulEl.innerHTML = listItems
+    
+}
