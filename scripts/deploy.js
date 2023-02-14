fcf60040f7449986094efaed843d5afa5aef7b68
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-02-14 18:00:02 +0530

    Added Mint file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,4 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+
 const hre = require("hardhat");
 
 async function main() {
@@ -13,8 +8,8 @@ async function main() {
 
   const lockedAmount = hre.ethers.utils.parseEther("1");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const MintExample = await hre.ethers.getContractFactory("MintExample");
+  const lock = await MintExample.deploy(unlockTime, { value: lockedAmount });
 
   await lock.deployed();
 
