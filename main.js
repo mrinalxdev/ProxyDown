e454123848bda9d5778fac8a0b8f34b5564f34b7
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-04-13 14:20:11 +0530

    Controls Object

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("myCanvas");
 canvas.height = window.innerHeight;
-canvas.width = 400;
+canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const car = new Car(100, 100, 30, 50);
