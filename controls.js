92c68cc8da5891b9bbfa9e62bcd7327550b4d8b3
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-04-13 15:14:41 +0530

    Car Movement

--- controls.js
+++ controls.js
@@ -24,7 +24,7 @@ class Controls {
           this.reverse = true;
           break;
       }
-      console.table(this)
+      
     };
 
     document.onkeyup = (event) => {
@@ -42,7 +42,6 @@ class Controls {
           this.reverse = false;
           break;
       }
-      console.table(this)
     };
   }
 }
